6a46fcd049e2cc3273d8f093c50561b3768a43e6
Joni <joni28492@gmail.com>
2020-12-31 19:26:20 +0100

    Primer Comit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//siempre busca un archivo .js
+import { saludar } from "./js/componentes";
+import './styles.css';
+
+
+const nombre='Joniiii';
+
+saludar(nombre);
+
---
+++ src/js/componentes.js
@@ -0,0 +1,10 @@
+import '../css/componentes.css';
+
+export const saludar =(nombre)=>{
+    console.log('Creando etiqueta h1, en el HTML');
+
+    const h1 = document.createElement('h1');
+    h1.innerText=`hola, ${nombre}`;
+    document.body.append(h1);
+}
+
